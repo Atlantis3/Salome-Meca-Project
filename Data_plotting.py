df201b4f8b0b3e3bc43fbccdb97e112592edcb14
AKRAM METAR <akrammetar01@gmail.com>
2024-07-28 15:35:05 +0200

    Added the non linear material behaviour to command file

--- Data_plotting.py
+++ Data_plotting.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # Load the data from the txt file
-df1 = pd.read_csv('/home/akram_metar/D_Drive/Salome_meca/Disk_geometry/disk_top_nodes.txt',skiprows=4,sep='\s+').groupby('NUME_ORDRE')
+df1 = pd.read_csv('/home/akram_metar/D_Drive/Salome_meca/Disk_geometry/disk_top_nodes_7.txt',skiprows=4,sep='\s+').groupby('NUME_ORDRE')
 
 # displacement of the nodes and extracting the load from all the nodes
 displacement = np.linspace(0,-0.15,31)
@@ -15,7 +15,7 @@ for count,value in enumerate(displacement):
 
 # Plotting the results 
 fig1,ax1 = plt.subplots()
-ax1.plot(displacement,df1_load,label='Disk Problem')
+ax1.plot(displacement,df1_load,label='Disk Problem',marker='o')
 ax1.set_title('Load Displacement Curve')
 ax1.set_xlabel('Deflection in mm')
 ax1.set_ylabel('Load in N')
